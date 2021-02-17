ffa83fa71555272e76092cf54235d57a46064397
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2021-02-17 11:21:11 +0530

    Add

---
+++ dna.py
@@ -0,0 +1,43 @@
+#python 3.7.1
+print("Enter any letter or symbol")
+"""".
+<!doctype html>
+<html>
+<head>
+<tit.....Just kidding.This has nothing to do with web!
+ In the input you are required to:
+
+enter a symbol e.g #,$,*,@...e.t.c
+
+finally leave a like cos this is my first python code (+👍)Thanks
+
+"""
+c=input()
+if len(c)>1:
+   c=c[0]
+if len(c)<1:
+   c="#"
+Turns=4
+s=" "
+def bids(a,b,c="",d=""):
+    return(a+b+c+d)
+
+def strand():
+    print(bids(s*19,c))
+    print(bids(s*17,c*4))
+    print(bids(s*16,c*6))
+    print(bids(s*14,c*2,s*6,c*2))
+    print(bids(s*12,c*14))
+    print(bids(s*11,c*2,s*12,c*2))
+    print(bids(s*11,c*16))
+    print(bids(s*12,c*2,s*10,c*2))
+    print(bids(s*14,c*10))
+    print(bids(s*16,c*2,s*2,c*2))
+    print(bids(s*17,c*4))
+    print(bids(s*18,c*2))
+
+for i in range(Turns):
+     strand()
+print(" ")
+print("First Python Code!please leave a like")
+
